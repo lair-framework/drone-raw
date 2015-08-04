acff2f010faab223e17163ba989b9239061ce193
Tom Steele <thomasjsteele@gmail.com>
2015-08-04 00:31:44 -0700

    Initial commit

---
+++ main.go
@@ -0,0 +1,118 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/url"
+	"os"
+	"strings"
+
+	"github.com/lair-framework/api-server/client"
+	"github.com/lair-framework/go-lair"
+)
+
+const (
+	version = "2.0.0"
+	tool    = "raw"
+	usage   = `
+Usage:
+  drone-raw <id> <filename>
+  export LAIR_ID=<id>; drone-raw <filename>
+Options:
+  -v              show version and exit
+  -h              show usage and exit
+  -k              allow insecure SSL connections
+  -force-ports    disable data protection in the API server for excessive ports
+  -tags           a comma separated list of tags to add to every host that is imported
+`
+)
+
+func main() {
+	showVersion := flag.Bool("v", false, "")
+	insecureSSL := flag.Bool("k", false, "")
+	forcePorts := flag.Bool("force-ports", false, "")
+	tags := flag.String("tags", "", "")
+	flag.Usage = func() {
+		fmt.Println(usage)
+	}
+	flag.Parse()
+	if *showVersion {
+		log.Println(version)
+		os.Exit(0)
+	}
+	lairURL := os.Getenv("LAIR_API_SERVER")
+	if lairURL == "" {
+		log.Fatal("Fatal: Missing LAIR_API_SERVER environment variable")
+	}
+	lairPID := os.Getenv("LAIR_ID")
+	var filename string
+	switch len(flag.Args()) {
+	case 2:
+		lairPID = flag.Arg(0)
+		filename = flag.Arg(1)
+	case 1:
+		filename = flag.Arg(0)
+	default:
+		log.Fatal("Fatal: Missing required argument")
+	}
+	u, err := url.Parse(lairURL)
+	if err != nil {
+		log.Fatalf("Fatal: Error parsing LAIR_API_SERVER URL. Error %s", err.Error())
+	}
+	if u.User == nil {
+		log.Fatal("Missing username and/or password")
+	}
+	user := u.User.Username()
+	pass, _ := u.User.Password()
+	if user == "" || pass == "" {
+		log.Fatal("Fatal: Missing username and/or password")
+	}
+	c, err := client.New(&client.COptions{
+		User:               user,
+		Password:           pass,
+		Host:               u.Host,
+		Scheme:             u.Scheme,
+		InsecureSkipVerify: *insecureSSL,
+	})
+	if err != nil {
+		log.Fatalf("Fatal: Error setting up client: Error %s", err.Error())
+	}
+	data, err := ioutil.ReadFile(filename)
+	if err != nil {
+		log.Fatalf("Fatal: Could not open file. Error %s", err.Error())
+	}
+	hostTags := []string{}
+	if *tags != "" {
+		hostTags = strings.Split(*tags, ",")
+	}
+	l := lair.Project{}
+	if err := json.Unmarshal(data, &l); err != nil {
+		log.Fatalf("Fatal: Could not parse JSON. Error %s", err.Error())
+	}
+	l.ID = lairPID
+
+	for i := range l.Hosts {
+		l.Hosts[i].Tags = append(l.Hosts[i].Tags, hostTags...)
+	}
+
+	res, err := c.ImportProject(&client.DOptions{ForcePorts: *forcePorts}, &l)
+	if err != nil {
+		log.Fatalf("Fatal: Unable to import project. Error %s", err)
+	}
+	defer res.Body.Close()
+	droneRes := &client.Response{}
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := json.Unmarshal(body, droneRes); err != nil {
+		log.Fatal(err)
+	}
+	if droneRes.Status == "Error" {
+		log.Fatalf("Fatal: Import failed. Error %s", droneRes.Message)
+	}
+	log.Println("Success: Operation completed successfully")
+}
